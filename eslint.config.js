0f55d9e9c9b88af9a0f097f01097ff9f4e0f96b2
Xat <i@xatloon.me>
2023-09-23 11:12:35 +0800

    chore: ⬆️ upgrade `@antfu/eslint-config`

--- eslint.config.js
+++ eslint.config.js
@@ -3,51 +3,52 @@ import astro from 'eslint-plugin-astro'
 import astroParser from 'astro-eslint-parser'
 import globals from 'globals'
 
-export default [
-  ...antfu,
-  {
-    ignores: [
-      '.astro/*',
-      'src/env.d.ts',
-    ],
-  },
-  {
-    files: ['**/*.astro'],
-    plugins: {
-      astro,
+export default antfu(
+  {},
+  [
+    {
+      ignores: [
+        '.astro/*',
+        'src/env.d.ts',
+      ],
     },
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...astro.environments.astro.globals,
-        ...globals.es2020,
+    {
+      files: ['**/*.astro'],
+      plugins: {
+        astro,
       },
-      parser: astroParser,
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.astro'],
-        sourceType: 'module',
+      languageOptions: {
+        globals: {
+          ...globals.node,
+          ...astro.environments.astro.globals,
+          ...globals.es2020,
+        },
+        parser: astroParser,
+        parserOptions: {
+          parser: '@typescript-eslint/parser',
+          extraFileExtensions: ['.astro'],
+          sourceType: 'module',
+        },
       },
-    },
-    rules: {
-      'astro/no-conflict-set-directives': 'error',
-      'astro/no-unused-define-vars-in-style': 'error',
-      'no-unused-vars': 'off',
-    },
-  },
-  {
-    files: ['**/*.astro/*.js', '*.astro/*.js'],
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.es2020,
-      },
-      parserOptions: {
-        sourceType: 'module',
+      rules: {
+        'astro/no-conflict-set-directives': 'error',
+        'astro/no-unused-define-vars-in-style': 'error',
+        'no-unused-vars': 'off',
       },
     },
-    rules: {
-      'prettier/prettier': 'off',
+    {
+      files: ['**/*.astro/*.js', '*.astro/*.js'],
+      languageOptions: {
+        globals: {
+          ...globals.browser,
+          ...globals.es2020,
+        },
+        parserOptions: {
+          sourceType: 'module',
+        },
+      },
+      rules: {
+        'prettier/prettier': 'off',
+      },
     },
-  },
-]
+  ])
