6ff529735e73f31cbf97cc16dc842d6c93de4b92
Xat <i@xatloon.me>
2023-09-27 20:42:11 +0800

    feat: ✨ add meta informations in post

--- astro.config.js
+++ astro.config.js
@@ -1,6 +1,7 @@
 /** @type {import('astro').AstroConfig} */
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import { readingTime } from './utils/reading-time.js'
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,4 +10,7 @@ export default defineConfig({
       applyBaseStyles: false,
     },
   )],
+  markdown: {
+    remarkPlugins: [readingTime],
+  },
 })
