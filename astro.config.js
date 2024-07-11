dabb85eae4f27003c64adefb01b1de12af8b338a
Xat <i@xatloon.com>
2024-07-11 16:59:58 +0800

    feat: add `vite-plugin-font` support

--- astro.config.js
+++ astro.config.js
@@ -1,12 +1,17 @@
 /** @type {import('astro').AstroConfig} */
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-import mdx from "@astrojs/mdx";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import viteFont from 'vite-plugin-font'
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
-    applyBaseStyles: false
-  }), mdx()]
-});
+    applyBaseStyles: false,
+  }), mdx()],
+  vite: {
+    plugins: [viteFont({
+      scanFiles: ['src/**/*', 'tailwind.config.js'],
+    })],
+  },
+})
