8e5143ae61e0dca090533571e96b4fbcba2250a9
Xat <i@xatloon.com>
2024-09-27 15:43:28 +0800

    refactor: remove `vite-plugin-font` cuz its bad compatibility with astro

--- astro.config.js
+++ astro.config.js
@@ -1,17 +1,14 @@
 /** @type {import('astro').AstroConfig} */
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
-import viteFont from 'vite-plugin-font'
-import mdx from '@astrojs/mdx'
 
-// https://astro.build/config
+import mdx from '@astrojs/mdx'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
+
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false,
-  }), mdx()],
-  vite: {
-    plugins: [viteFont({
-      scanFiles: ['src/**/*'],
-    })],
-  },
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    mdx(),
+  ],
 })
